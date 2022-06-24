d4b75ca5ba8baf45d08347c9b5ff7eeed87f0d61
Max Kottong <max.kottong@gmail.com>
2022-06-24 14:04:00 -0400

    Added initial header and body elements for homepage, as well as some styling to them

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
-// JavaScript source code
+var questions = [
+    {
+        question: "What is a question?",
+        answerChoices: ["1", "2", "3", "4"],
+        answer: 2
+    },
+    {
+        question: "What is a question?",
+            answerChoices: ["1", "2", "3", "4"],
+            answer: 2
+    },
+    {
+        question: "What is a question?",
+            answerChoices: ["1", "2", "3", "4"],
+            answer: 2
+    },
+    {
+        question: "What is a question?",
+            answerChoices: ["1", "2", "3", "4"],
+            answer: 2
+    },
+    {
+        question: "What is a question?",
+            answerChoices: ["1", "2", "3", "4"],
+            answer: 2
+    }
+];
