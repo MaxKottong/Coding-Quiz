fb18c68e0b3df04f6411ba4ccd4ca57fdf487e2e
Max Kottong <max.kottong@gmail.com>
2022-06-24 15:16:07 -0400

    Added in basic outline for homepage and where each button will link to

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,3 +1,4 @@
+
 var questions = [
     {
         question: "What is a question?",
@@ -6,22 +7,22 @@ var questions = [
     },
     {
         question: "What is a question?",
-            answerChoices: ["1", "2", "3", "4"],
-            answer: 2
+        answerChoices: ["1", "2", "3", "4"],
+        answer: 2
     },
     {
         question: "What is a question?",
-            answerChoices: ["1", "2", "3", "4"],
-            answer: 2
+        answerChoices: ["1", "2", "3", "4"],
+        answer: 2
     },
     {
         question: "What is a question?",
-            answerChoices: ["1", "2", "3", "4"],
-            answer: 2
+        answerChoices: ["1", "2", "3", "4"],
+        answer: 2
     },
     {
         question: "What is a question?",
-            answerChoices: ["1", "2", "3", "4"],
-            answer: 2
+        answerChoices: ["1", "2", "3", "4"],
+        answer: 2
     }
 ];
